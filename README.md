a363caed7336b6bb1c07867b48c862cfac3ae9c8
Adam Malec <add.malec@gmail.com>
2026-02-09 19:08:08 +0300

    build: add vite to project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Landing Page like personal Diana Kaspiarovich website.
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+export default {
+  css: {
+    transformer: 'lightningcss',
+  },
+  build: {
+    cssMinify: 'lightningcss'
+  }
+};
