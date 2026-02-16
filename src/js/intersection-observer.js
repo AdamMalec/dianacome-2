248dbe2869a1f46e11efa4f7d2eff68cf1cbec4a
Adam Malec <add.malec@gmail.com>
2026-02-16 21:18:14 +0300

    fix(js): correct the timeline appearance from bottom to top

--- src/js/intersection-observer.js
+++ src/js/intersection-observer.js
@@ -1,7 +1,7 @@
 const serviceEls = document.querySelectorAll(".service");
 const reviewEls = document.querySelectorAll(".review");
 const timelineEls = document.querySelectorAll(".timeline-content");
-let tlIndex = 0;
+const timelineCounterEls = document.querySelectorAll(".timeline-circle");
 
 const observerOptions = {
 	rootMargin: "0% 0% -25% 0%",
@@ -28,20 +28,21 @@ const observerReviews = new IntersectionObserver((entries) => {
 const observerTimeline = new IntersectionObserver((entries) => {
 	for (const entry of entries) {
 		if (entry.isIntersecting) {
+			let tlIndex = [...timelineEls].findIndex( item => item === entry.target);
+
 			if (tlIndex % 2 !== 0) {
 				entry.target.classList.add("animate-fade-slide-left");
 				observerTimeline.unobserve(entry.target);
 				setTimeout(() => {
-					entry.target.nextElementSibling.firstElementChild.style.backgroundColor = 'var(--color-bg-timeline)';
+					timelineCounterEls[tlIndex].style.backgroundColor = 'var(--color-bg-timeline)';
 				}, 500);
 			} else {
 				entry.target.classList.add("animate-fade-slide-right");
 				observerTimeline.unobserve(entry.target);
 				setTimeout(() => {
-					entry.target.previousElementSibling.firstElementChild.style.backgroundColor = 'var(--color-bg-timeline)';
+					timelineCounterEls[tlIndex].style.backgroundColor = 'var(--color-bg-timeline)';
 				}, 500);
 			}
-			tlIndex++;
 		}
 	}
 }, observerOptions);
